1035590b15599c9acf4310c6ef88e3c458d896b9
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-09 19:37:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 function distanceFromHqInBlocks(someValue){
   let blocks;
   if (someValue>42){
-  blocks= someValue-42;
-  }
-  else 
-  {
-  blocks=42-someValue;
-  }
+    blocks= someValue-42;
+    }
+  else {
+    blocks=42-someValue;
+    }
   return blocks;
 }
